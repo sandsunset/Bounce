5dbac6cea8dd9923b2bd1ca0628b0c7377ae020e
Sandsunset <sejoon0606@gmail.com>
2021-02-09 10:42:24 +0900

    feat: add spead to ball

--- static/main.js
+++ static/main.js
@@ -6,7 +6,7 @@ function newObj(event) {
   let x = event.clientX
   let y = event.clientY
   
-  let newObject = {'x': x, 'y':y, 'speedX' :10, 'speedY': 10};
+  let newObject = {'x': x, 'y':y, 'speedX' :1, 'speedY': 1};
 
   objArray.push(newObject);
 
@@ -14,14 +14,19 @@ function newObj(event) {
 }
 
 function render() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
     for (let object of objArray) {
         let X = object['x'];
         let Y = object['y'];
 
+        let speedX = object['speedX'];
+        let speedY = object['speedY'];
         drawObj(X,Y);
+
+        object['x'] = X + speedX;
+        object['y'] = Y + speedY;
     }
 
-    ctx.clearRect(0,0,canvas.width,canvas.height);
     window.requestAnimationFrame(render);
 }
 
